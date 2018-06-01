b7f894e775a1b0058acea26268e7f9795619529a
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 02:19:12 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ array = []
+ 
+ while i < collection.length
+ if yield(collection[i]) == true
+   array.push(collection[i])
+ end
+ i += 1
+ end
+ 
+ array
 end
